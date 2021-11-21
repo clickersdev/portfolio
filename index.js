41f18393cedc10964f4fd5a7bff5c99a936b7dfe
Tarik Kaya <clickers331@gmail.com>
2021-11-21 11:58:48 +0300

    Fixed stuff

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ console.log(myNav)
 document.addEventListener("scroll", event => {
     if(window.scrollY > 15){
         myNav.classList.add('scroll')
-        console.log(window.scroll)
     } else {
         myNav.classList.remove('scroll')
     }
